dfa78dc85ea173e51ca6e1e55b61818852f9af69
Andy Wendt <andy@awendt.com>
2015-06-20 13:56:46 -0600

    initial commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(288, 205);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private int pomodoroLength = 60 * 25;
+        private int elapsed;
+        private int timeLeft;
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetTime();
+            setLabel1Initial();
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            button1.Text = "Start";
+            
+            button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                setLabel1Initial();
+                timer1.Enabled = false;
+                button1.Text = "Start";
+            } else
+            {
+                resetTime();
+                timer1.Enabled = true;
+                timer1_Tick(this, EventArgs.Empty);
+                button1.Text = "Reset";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft = pomodoroLength - ++elapsed;
+            setLabel1();
+        }
+
+        private void setLabel1()
+        {
+            TimeSpan time = TimeSpan.FromSeconds(timeLeft);
+            label1.Text = string.Format("{0}:{1:00}", time.Minutes, time.Seconds);
+        }
+
+        private void setLabel1Initial()
+        {
+            label1.Text = "25:00";
+        }
+
+        private void resetTime()
+        {
+            elapsed = 0;
+            timeLeft = pomodoroLength;
+        }
+    }
+}
