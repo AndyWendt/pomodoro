4c503a7f24cf4aa0a254b9fecbcfb35266a4a675
Andy Wendt <andy@awendt.com>
2015-06-20 15:58:40 -0600

    fixed error with ns and class not found
    closes #6

--- WindowsFormsApplication2/Program.cs
+++ WindowsFormsApplication2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication2
+namespace Pomodoro
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace WindowsFormsApplication2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Pomodoro());
         }
     }
 }
